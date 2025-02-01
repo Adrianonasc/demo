3d52f0de8fe6f62f2f3d8465c945ed26a8b8312b
Adriano Nascimento Lima <adriano.nasc@hotmail.com>
2025-02-01 20:26:34 -0300

    estrutura base

--- src/main/java/com/it/empresas/controller/PessoasController.java
+++ src/main/java/com/it/empresas/controller/PessoasController.java
@@ -1,5 +1,10 @@
 package com.it.empresas.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/empresa")
 public class PessoasController {
   
 }
