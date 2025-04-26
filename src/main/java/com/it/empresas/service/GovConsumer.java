d2b57dc9059e069ead9f1bf48df3d085fe808ccb
Adrianonasc <tel_ct@hotmail.com>
2025-04-26 17:15:02 -0300

    remoção de lib

--- src/main/java/com/it/empresas/service/GovConsumer.java
+++ src/main/java/com/it/empresas/service/GovConsumer.java
@@ -2,6 +2,7 @@ package com.it.empresas.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,9 +10,6 @@ import com.it.empresas.model.response.VigenciasResponse;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -22,17 +20,21 @@ public class GovConsumer {
   private ObjectMapper objectMapper;
 
   public VigenciasResponse consultaReceitaFederal(String cnpj) {
-    OkHttpClient client = new OkHttpClient();
-    Request request = new Request.Builder()
-        .url("https://brasilapi.com.br/api/cnpj/v1/" + cnpj)
-        .build();
-    try (Response response = client.newCall(request).execute()) {
-      JsonNode jsonNodeRoot = objectMapper.readTree(response.body().string());
-      VigenciasResponse vigenciasResponse = new VigenciasResponse(0L, null, jsonNodeRoot.get("cnpj").asText(),
-          jsonNodeRoot.get("razao_social").asText());
+    RestClient defaultClient = RestClient.create();
+    var vigenciasResponse = defaultClient
+                              .get()
+                              .uri("https://brasilapi.com.br/api/cnpj/v1/" + cnpj)
+                              .exchange((request, response) -> {
+                              if (response.getStatusCode().is4xxClientError()) {
+                                throw new RuntimeException("Erro ao consultar receita federal ");
+                              }
+                              else {
+                                  JsonNode jsonNodeRoot = objectMapper.readTree(response.bodyTo(String.class));
+                                  VigenciasResponse _vigenciasResponse = new VigenciasResponse(0L, null, jsonNodeRoot.get("cnpj").asText(),
+                                                                              jsonNodeRoot.get("razao_social").asText());
+                                  return  _vigenciasResponse;
+                              }
+    });
       return vigenciasResponse;
-    } catch (Exception e) {
-      throw new RuntimeException("Erro ao consultar receita federal", e);
-    }
   }
 }
